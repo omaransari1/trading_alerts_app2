39f9ca9bde68336eb58e152d977503855d1b6c9c
Omar Ansari <omaransari1@gmail.com>
2017-08-14 00:49:00 -0500

    Set up requests to Quandl API instead of AlphaVantage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,19 +7,22 @@ class PagesController < ApplicationController
 
          
 
-      conn = Faraday.new(:url => 'https://www.alphavantage.co') do |faraday|
+      conn = Faraday.new(:url => 'https://www.quandl.com') do |faraday|
         faraday.request  :url_encoded             # form-encode POST params
         faraday.response :logger                  # log requests to STDOUT
         faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
       end
 
-    @stocks.each do |stock|
-      response = conn.get "/query?function=TIME_SERIES_DAILY&symbol=#{stock.symbol}&outputsize=full&apikey=JG3NSS22E1GK9BB5"
+    # @stocks.each do |stock|  **replace MSFT with #{stock.symbol} for loop
+      response = conn.get "/api/v3/datasets/WIKI/MSFT.json?limit=50&apikey=hMsC5ogvjGQTiy5eYTsR"
 
       @response = response.body
       @parsed_response = JSON.parse @response
-      @closing_prices_arr << @parsed_response["Time Series (Daily)"]["2017-08-02"]["4. close"]
-    end
+
+      @daily_close = @parsed_response["dataset"]["data"][0][4]
+
+      # @closing_prices_arr << @parsed_response["Time Series (Daily)"]["2017-08-02"]["4. close"]
+    # end
 
 
     render 'home'
