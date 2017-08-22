ab0423e858cf1e98c9d01dc4e9172370ae436ab8
Casey Platt <kse0824@gmail.com>
2017-08-22 15:08:50 -0500

    loop through all stocks, names, closing price

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,24 +1,24 @@
 class PagesController < ApplicationController
-  def home
-
+  def index
     @stocks = Stock.all
-
     @closing_prices_arr = []
-
+    render 'index.html.erb'
+  end
+end 
          
 
       conn = Faraday.new(:url => 'https://www.quandl.com') do |faraday|
         faraday.request  :url_encoded             # form-encode POST params
         faraday.response :logger                  # log requests to STDOUT
         faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
-      end
+    
 
-<<<<<<< HEAD
-    @stocks.each do |stock|
+
+      @stocks.each do |stock|
       response = conn.get "/query?function=TIME_SERIES_DAILY&symbol=#{stock.symbol}&outputsize=full&apikey=YMLLXVLX6UQ6SBEM"
-=======
+
     response = conn.get "/api/v3/datasets/WIKI/AAPL.json?limit=50&apikey=hMsC5ogvjGQTiy5eYTsR"
->>>>>>> 9fb0e73af81b0aea035ddf3f208e2262841abc7d
+      ## 9fb0e73af81b0aea035ddf3f208e2262841abc7d
 
     @response = response.body
     @parsed_response = JSON.parse @response
@@ -31,7 +31,7 @@ class PagesController < ApplicationController
     for i in (0..49)
       @fifty_day_sum += @parsed_response["dataset"]["data"][i][11].to_f
     end
-    @fifty_day_MA = @fifty_day_sum / 50
+      @fifty_day_MA = @fifty_day_sum / 50
 
 
 
